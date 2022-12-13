191498616786ae120120968838576aa93cc58e56
ca r215.08 <ca.r215.08@ca.local>
2022-12-13 01:47:57 -0800

    First commit

---
+++ WeaponTask/Models/Weapon.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeaponTask.Models
+{
+    internal class Weapon
+    {
+        int _capacity;
+        int _bulletnumber;
+        int _endsecond;
+        string _shootmodd;
+        public int Capacity 
+        {
+            get => _capacity;
+            set
+            {
+                if (value > 0)
+                {
+                    _capacity= value;
+                };
+            } 
+        }
+        public int BulletNumber 
+        {
+            get => _bulletnumber;
+            set
+            {
+                if (value > 0 && value < Capacity)
+                {
+                    _bulletnumber = value;
+                }
+            }
+             
+        }
+        public int EndSecond { get; set; }
+        public string ShootMood { get; set; }
+
+        public Weapon(int capacity,int bulletnumber,int endsecond,string shootmood)
+        {
+            Capacity= capacity;
+            BulletNumber= bulletnumber;
+            EndSecond= endsecond;
+            ShootMood= shootmood;
+        }
+        public int Shoot()
+        {
+            if (BulletNumber > 0)
+            {
+                
+                int shoot = BulletNumber - 1;
+                BulletNumber--;
+                return shoot;
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+        public int Fire()
+        {
+            if (ShootMood == "avtomatik")
+            {
+                int a = BulletNumber / 5;
+                return a;
+            }
+            else
+            {
+
+                return 0;
+            }
+        }
+        public int GetRemainBulletCount()
+        {
+            return Capacity - BulletNumber;
+        }
+        public void ChangeFireMethod()
+        {
+            if (ShootMood == "tek")
+            {
+                ShootMood = "avtomatik";
+                Console.WriteLine("Atis modunuz tekliden avtomatike cevrildi");
+            }
+            else if (ShootMood == "avtomatik")
+            {
+                ShootMood = "tek";
+                Console.WriteLine("Atis modunuz avtomatikden tek moda cevrildi");
+            }
+        }
+        public void Reload()
+        {
+            if (Capacity == BulletNumber)
+            {
+                Console.WriteLine("Sizin daraginiz tamamile doludur.Ona gore de yeniden doldurulmadi");
+            }
+            else
+            {
+                int AddBullet = Capacity- BulletNumber;
+                Console.WriteLine("Sizin daraga " + AddBullet + " gulle elave edilerek " + Capacity + " oldu" );
+                BulletNumber = Capacity;
+            }
+        }
+
+
+
+    }
+}
---
+++ WeaponTask/Program.cs
@@ -0,0 +1,75 @@
+﻿using WeaponTask.Models;
+
+namespace WeaponTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Silahinizin daraginin tuta bileceyi maximum gulle sayini daxil edin : ");
+            int capacity = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Silahinizin daraginda olan gullelerin sayini daxil edin : ");
+            int bulletnum = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Silahinizin daraginin bosalma saniyesini daxil edin : ");
+            int endsec = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Silahinizin moodunu daxil edin (tek || avtomatik) : ");
+            string mood = Convert.ToString(Console.ReadLine());
+            Weapon weapon = new Weapon(capacity, bulletnum, endsec, mood);
+
+
+            bool repeat = true;
+            do
+            {
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine("Informasiya almaq ucun 0 daxil edin ");
+                byte input1 = Convert.ToByte(Console.ReadLine());
+                if (input1 == 0)
+                {
+                    Console.WriteLine("1 - Shoot metodu ucun \n2 - Fire metodu ucun\n3 - GetRemainBulletCount metodu ucun\n4 - Reload metodu ucun\n" +
+                    "5 - ChangeFireMode metodu ucun\n6 - Proqramdan dayandırmaq ucun");
+                    byte input = Convert.ToByte(Console.ReadLine());
+                    switch (input)
+                    {
+                        case 1:
+                            if (weapon.Capacity > 0)
+                            {
+                                Console.WriteLine("Silahinizdan 1 el ates olundu geride qalan gulle sayiniz : " + weapon.Shoot() );
+                            }
+                            else
+                            {
+                                Console.WriteLine("Daragda gulle olmadigi ucun ates acilmadi,Reload edin\"");
+                            }
+                            break;
+                        case 2:
+                            Console.WriteLine("Silahinizin butun gulleleri atesleme muddeti " + weapon.Fire());
+                            break;
+                        case 3:
+                            Console.WriteLine("Silahinizin daraginda " + weapon.BulletNumber + " gulle var ve tam dolmasi ucun " + weapon.GetRemainBulletCount() + " gulle lazimdir");
+                            break;
+                        case 4:
+                            weapon.Reload();
+                            break;
+                        case 5:
+                            weapon.ChangeFireMethod();
+                            break;
+                        case 6:
+                            repeat = false ;
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("0 daxil etmeyiniz xahis olunur");
+                }
+
+            } while (repeat);
+
+        
+            
+
+
+        }
+    }
+}
